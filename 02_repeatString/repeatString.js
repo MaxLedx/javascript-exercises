04f78171245c081f36f16c7508c59253c7c5cfe1
MaxLedx <80272454+MaxLedx@users.noreply.github.com>
2025-04-28 16:16:40 +0200

    Finish 1 to 7

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+    if (num < 0) return 'ERROR';
+    let builder = '';
+    for (let i = 0; i < num; i++) {
+        builder += string;
+    }
+    return builder;
 };
 
 // Do not edit below this line
